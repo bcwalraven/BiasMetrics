d4d0a22ffc1d7fad6d73ccf65b35b5505054069d
Brandon Walraven <brandonwalraven@Brandons-MacBook-Pro.local>
2019-06-06 13:38:31 -0400

    release 0.4.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'biasMetrics',      
   packages = ['biasMetrics'],
-  version = '0.3.5',      
+  version = '0.4.0',      
   license='MIT',        
   description = 'Set of classification metrics to determine bias in underrepresented subpopulations.',  
   author = 'Brandon Walraven',
   author_email = '',      
   url = 'https://github.com/bcwalraven',
-  download_url = 'https://github.com/bcwalraven/biasMetrics/archive/v0.3.5.tar.gz',    
+  download_url = 'https://github.com/bcwalraven/biasMetrics/archive/v0.4.0.tar.gz',    
   keywords = ['AUC', 'Classification', 'Metrics'],   
   install_requires=[           
           'numpy',
